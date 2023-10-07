1d655ee7f46dfdcac51e86c263de52405509fcea
Tega2Praise <egwabortega@gmail.com>
2023-10-07 09:53:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
-#!/bin/bash
-gcc puts "Programming is like building a multilingual puzzle"
-0
+#include <stdio.h>
+
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
