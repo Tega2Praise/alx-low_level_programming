3ed6384b18425528cf7050911a15000f90648d4e
Tega2Praise <egwabortega@gmail.com>
2023-10-09 18:06:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-
+ #include <unistd.h>
 /**
- * main - Entry point
- * Description: prints "\"and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * Return: 1
+ * main - this prints exactly the input on it
+ * Return: always 1.
  */
-
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
