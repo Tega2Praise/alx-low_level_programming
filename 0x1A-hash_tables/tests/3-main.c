563cffc4ec80ba3233ece9fbed6da890475b80e2
Tega2Praise <egwabortega@gmail.com>
2024-02-02 15:14:43 -0800

    0x1A-hash project

---
+++ 0x1A-hash_tables/tests/3-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "../hash_tables.h"
+
+/**
+ * main - check the code for ALX-Africa Cohort 5 Students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+
+	
